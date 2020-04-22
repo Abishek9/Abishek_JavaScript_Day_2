a7380049eb3ac532515227d84b7eca593a2fcc91
Abishek9 <61542313+Abishek9@users.noreply.github.com>
2020-04-22 20:15:45 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,102 @@
+// show the title
+
+// let name="Preetha"
+// let price = `Hello ${name}` ;
+//  price = price.toLowerCase();
+
+// let price={
+//     firstName:'Abishek',
+//     lastName:'Preetha'
+// };
+let price = 100;
+// if(+(1.1+1.1).toFixed(2) == 2.2){
+//     showMessage('Discount');
+// }else{
+//     showMessage('No Discount');
+// }
+// let message = (price > 10)? 'Yes' : 'No';
+// showMessage(message);
+// for(let i=0;i<3;i++)
+// {
+//     console.log(i);
+// }
+ 
+/*
+    Detail complex logic...
+    Some algorithm...
+
+*/
+// console.log(price);
+
+
+// let fun = function logMessage(){
+//     console.log("Here is my First Function");
+// }
+// function logMessage(Message, anotherMessage){
+//     console.log("Here is my First1 Functions");
+// }
+
+// function mathMessage(value){
+//     let fun1=(value*2);
+    
+// }
+// let key=42;
+// function msg(value){
+    
+//     let msg1 = function(){
+//         let key = 14;
+//         console.log(key);
+//         return key;
+//     }
+
+//     let msg2 = value* msg1();
+//     console.log(key);
+//     return msg2;
+
+// }
+// let ouput=msg(2);
+// showMessage(output);
+
+// changePercentageoff(40);
+
+// let person = {
+//     name:'Abi',
+//     showInfo: function(){
+//         showMessage(this.name);
+//     }
+// };
+// person['name']='Preetha';
+// showMessage(person.name);
+// 
+// let value= new Date();
+// showMessage(value.toDateString());
+
+// const header  = document.getElementById('see-review');
+
+// header.addEventListener('click',function(){
+//    const header1= document.getElementById('review');
+//    header1.classList.remove('d-none')
+// });
+
+// const values=['a','b','c'];
+// values.push('d');
+// values.pop();
+// values.shift();
+// values.unshift('P');
+
+// console.log(values);
+
+// const values=['a','b','c','d','e'];
+// const slice=values.slice(1,3);
+// console.log(values);
+// console.log(slice);
+
+// const values=['a','b','c','d','e'];
+
+// const set=values.filter(function(item){
+// return item > 'b';
+// });
+// console.log(set);
+
+const con= document.getElementsByClassName('container');
+console.log(con);
